9cd8249c10a1cb9b63e3ffcdb937def65c272eff
Will Morrison <camocrazed+github@gmail.com>
2021-05-13 13:09:04 -0400

    Add generator and assets

---
+++ src/generator_model.py
@@ -0,0 +1,31 @@
+# Basic 1kW generator
+
+class Generator:
+
+  _MAX_OUTPUT = 1000
+
+  def __init__(self):
+    self._connected = False
+  
+  @property
+  def name(self):
+    return "1kW Perpetual motion generator"
+
+  @property
+  def nameplate_capacity(self):
+    return _MAX_OUTPUT
+
+  @property
+  def current_output(self):
+    return _MAX_OUTPUT if self._connected else 0
+
+  @property
+  def connected(self):
+    return self._connected
+
+  def toggle_connection(self):
+    self._connected = not self._connected
+
+  def tick(self, environment):
+  """Updates internal state every game tick."""
+    pass
